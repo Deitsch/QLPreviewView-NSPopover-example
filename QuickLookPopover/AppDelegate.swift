e6a25fc3225b69beeea9909f6123155e359d2625
Simon Deutsch <simon.deutsch97@gmail.com>
2021-09-08 12:32:03 +0200

    fixed panel runtime warning

--- QuickLookPopover/AppDelegate.swift
+++ QuickLookPopover/AppDelegate.swift
@@ -10,10 +10,10 @@ import Cocoa
 import Quartz
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate, QLPreviewPanelDataSource {
+class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-    var url: URL!
+    let url: URL! = Bundle.main.url(forResource: "Yosemite", withExtension: "jpg")
 
     @IBAction func showPopover(_ sender: AnyObject) {
         let popover = NSPopover()
@@ -23,34 +23,39 @@ class AppDelegate: NSObject, NSApplicationDelegate, QLPreviewPanelDataSource {
     }
     
     @IBAction func showPanel(_ sender: Any) {
-        if QLPreviewPanel.sharedPreviewPanelExists() && QLPreviewPanel.shared().isVisible {
-            QLPreviewPanel.shared().orderOut(nil)
+        guard let panel = QLPreviewPanel.shared() else { return }
+        
+        if QLPreviewPanel.sharedPreviewPanelExists() && panel.isVisible {
+            panel.orderOut(nil)
         }
         else {
-            url = Bundle.main.url(forResource: "Yosemite", withExtension: "jpg")
-            guard let panel = QLPreviewPanel.shared() else { return }
-            panel.dataSource = self
             panel.makeKeyAndOrderFront(nil)
         }
     }
-    
-    
+}
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
+extension AppDelegate: QLPreviewPanelDataSource {
     
     func numberOfPreviewItems(in panel: QLPreviewPanel!) -> Int {
         return 1
     }
 
     func previewPanel(_ panel: QLPreviewPanel!, previewItemAt index: Int) -> QLPreviewItem! {
-        return self.url as QLPreviewItem
+        return url as QLPreviewItem
+    }
+    
+    override func acceptsPreviewPanelControl(_ panel: QLPreviewPanel!) -> Bool {
+        return true
+    }
+    
+    override func beginPreviewPanelControl(_ panel: QLPreviewPanel!) {
+        panel.dataSource = self
+        panel.delegate = self
+        panel.currentPreviewItemIndex = 0
     }
 
+    override func endPreviewPanelControl(_ panel: QLPreviewPanel!) {
+        panel.dataSource = nil
+        panel.delegate = nil
+    }
 }
-
