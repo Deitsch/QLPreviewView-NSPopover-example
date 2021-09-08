f28e1efbebc7031d79f23331264ffeab9deef3ff
Simon Deutsch <simon.deutsch97@gmail.com>
2021-09-08 09:30:14 +0200

    addded panel button

--- QuickLookPopover/AppDelegate.swift
+++ QuickLookPopover/AppDelegate.swift
@@ -10,15 +10,30 @@ import Cocoa
 import Quartz
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, QLPreviewPanelDataSource {
 
     @IBOutlet weak var window: NSWindow!
+    var url: URL!
 
     @IBAction func showPopover(_ sender: AnyObject) {
         let popover = NSPopover()
         popover.contentViewController = ViewController()
         popover.show(relativeTo: NSZeroRect, of: window.contentView!, preferredEdge: .maxX)
     }
+    
+    @IBAction func showPanel(_ sender: Any) {
+        if QLPreviewPanel.sharedPreviewPanelExists() && QLPreviewPanel.shared().isVisible {
+            QLPreviewPanel.shared().orderOut(nil)
+        }
+        else {
+            url = Bundle.main.url(forResource: "Yosemite", withExtension: "jpg")
+            guard let panel = QLPreviewPanel.shared() else { return }
+            panel.dataSource = self
+            panel.makeKeyAndOrderFront(nil)
+        }
+    }
+    
+    
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -26,6 +41,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    func numberOfPreviewItems(in panel: QLPreviewPanel!) -> Int {
+        return 1
+    }
+
+    func previewPanel(_ panel: QLPreviewPanel!, previewItemAt index: Int) -> QLPreviewItem! {
+        return self.url as QLPreviewItem
     }
 
 }
